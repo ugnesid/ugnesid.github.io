650a968ca33e54a53bab4c789297f130d3521487
Ugne Sidlauske <ugnele9@gmail.com>
2022-11-15 10:26:00 -0700

    updateJs

--- Tips.js
+++ Tips.js
@@ -1,27 +1,6 @@
-//let noOfCharac = 150;
-//let contents = document.querySelectorAll(".contentp2");
-//
-//contents.forEach(contentp2 => {
-    //if(contentp2.textContent.length < noOfCharac) {
-       // contentp2.nextElementSibling.style.display = "none";
-    //}
-    //else{
-    //    let displayText = contentp2.textContent.slice(0, noOfCharac);
-    //    let moreText = contentp2.textContent.slice(noOfCharac);
-    //    contentp2.innerHTML = `${displayText}<span class="dots">...</span><span class="hide more">${moreText}</span>`;
-   // }
-//});
-//function readMore(btn){
-   // let post = btn.parentElement;
-   // post.querySelector(".dots").classList.toggle("hide");
-   // post.querySelector(".more").classList.toggle("hide");
-   // btn.textContent == "Read More" ? btn.textContent = "Read Less" : btn.textContent = "Read More";
-//}
-const list = document.getElementById("list");
-
-function readMore(btn) {
+const readMore = function(btn) {
     btn.textContent = changeText(btn.textContent);
-    list.classList.toggle("hide");
+    btn.previousElementSibling.classList.toggle("hide");
 }
 
 function changeText(txt) {
@@ -30,3 +9,4 @@ function changeText(txt) {
     }
     return "Read More";
 };
+
